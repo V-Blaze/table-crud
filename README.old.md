126e07d3ef31632883d63589667a26e6b793b04c
V-Blaze <valentine6586@gmail.com>
2023-01-02 18:05:00 +0100

    Setup react and project dependencies

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# table-crud
+Table-crud is an application that receives user input data and displays it on a table.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="relative sm:-8 p-4 bg-[#4369B2] min-h-screen flex flex-row">
+      <h2>Hello</h2>
+  </div>
+  )
+}
+
+export default App
